65983256af6a06a524982d67017a2de2eea4623d
Alvaro Lavado <lavado.mejia.alvaro.elias@gmail.com>
2026-04-25 18:09:39 -0500

    cree una clase

---
+++ src/main/java/com/mycompany/patolab2/Patolab2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.patolab2;
+
+/**
+ *
+ * @author sistemas
+ */
+public class Patolab2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
