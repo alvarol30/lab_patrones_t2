e2c0b4a3ab15b3c723bbbca3445710238a3aa679
lavad <lavad@AlvaroL>
2026-04-26 06:08:06 -0500

    termine el main para cumpla con el protocolo SOLID

--- src/main/java/com/mycompany/patolab2/SimPato.java
+++ src/main/java/com/mycompany/patolab2/SimPato.java
@@ -1,16 +1,40 @@
 package com.mycompany.patolab2;
 public class SimPato{
 public static void main(String[] args){
+    //creo los objetos de lls patos
     PatoSenuelo ps1 = new PatoSenuelo();
     PatoDeGoma pg1 = new PatoDeGoma();
     PatoCabezaRoja pcr1 = new PatoCabezaRoja();
     PatoReal pr1 = new PatoReal();
-        pr1.mostrar();
-        pr1.realizarCuaqueo();
-        pr1.realizarVuelo();
-        pr1.nadar();
-
-        System.out.println();
+    
+    //llamamos a los objetos con sus metodos
+    //PATO REAL
+    pr1.mostrar();
+    pr1.realizarCuaqueo();
+    pr1.realizarVuelo();
+    pr1.nadar();
+    System.out.println();
+    
+    //PATO DE GOMA
+    pg1.mostrar();
+    pg1.realizarCuaqueo();
+    pg1.realizarVuelo();
+    pg1.nadar();
+    System.out.println();
+    
+    //PATO SEÑUELO
+    ps1.mostrar();
+    ps1.realizarCuaqueo();
+    ps1.realizarVuelo();
+    ps1.nadar();
+    System.out.println();
+    
+    //PATO DE CABEZA ROJA
+    pcr1.mostrar();
+    pcr1.realizarCuaqueo();
+    pcr1.realizarVuelo();
+    pcr1.nadar();
+    System.out.println();
     }
     
 }
